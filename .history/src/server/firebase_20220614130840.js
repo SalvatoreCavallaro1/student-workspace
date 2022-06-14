f33900043f09219bcfde290172ccf6e28050c6ae
salvocav <salvocavallaro.dr@gmail.com>
2022-06-14 13:18:39 +0200

    firebaseConfig

---
+++ .history/src/server/firebase_20220614130840.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6srdGfxfUv54zn4mJY4MnykSyIlIfvHs",
+    authDomain: "student-workspace-9481a.firebaseapp.com",
+    projectId: "student-workspace-9481a",
+    storageBucket: "student-workspace-9481a.appspot.com",
+    messagingSenderId: "1066639130473",
+    appId: "1:1066639130473:web:d73de51e0d5ebfdf7147f8",
+    measurementId: "G-7Z0NWVCVDV"
+  };
---
+++ .history/src/server/firebase_20220614131318.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/database';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6srdGfxfUv54zn4mJY4MnykSyIlIfvHs",
+    authDomain: "student-workspace-9481a.firebaseapp.com",
+    projectId: "student-workspace-9481a",
+    storageBucket: "student-workspace-9481a.appspot.com",
+    messagingSenderId: "1066639130473",
+    appId: "1:1066639130473:web:d73de51e0d5ebfdf7147f8",
+    measurementId: "G-7Z0NWVCVDV"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
